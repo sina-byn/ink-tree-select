69192ca7281d47e9d706349f12e56dda60dc2457
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-04 20:38:15 +0330

    chore: barrel file updated to export required types and components

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,11 @@ export {
   type TreeSelectProps,
   type TreeSelectOptions,
 } from './components/TreeSelect.js';
+
+export {
+  VirtualTreeSelect,
+  type VirtualTreeSelectProps,
+  type VirtualTreeSelectOptions,
+} from './components/VirtualTreeSelect.js';
+
+export { type Tree } from './utils/index.js';
