34e5debb7e5cff545c7ce699690575c7f9776061
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-04 09:49:17 +0330

    chore: createDirectoryTree util func added

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Box, render } from 'ink';
 
+// * utils
+import { createDirectoryTree } from './utils/index.js';
+
 const App = () => {
+  console.log(createDirectoryTree('./', '.'));
+
   return <Box></Box>;
 };
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,6 +4,9 @@ import fs from 'fs';
 import fastGlob from 'fast-glob';
 const { globSync: fg } = fastGlob;
 
+// * types
+type Tree = { name: string; dir?: boolean; branches: Tree[] };
+
 export const readDirectory = (root: string, ignore: string[] = []) => {
   if (!fs.existsSync(root)) throw new Error(`Could not find '${root}'`);
   if (!fs.statSync(root).isDirectory()) throw new Error(`'${root}' is not a directory`);
@@ -19,3 +22,26 @@ export const readDirectory = (root: string, ignore: string[] = []) => {
   return paths;
 };
 
+export const createDirectoryTree = (root: string, rootAlias: string) => {
+  const paths = readDirectory(root);
+  const tree: Tree = { name: rootAlias, dir: true, branches: [] };
+
+  for (const path of paths) {
+    const pathChunks = path.replace(root, '').split('/').filter(Boolean);
+    const isDir = path.endsWith('/');
+    let subTree = tree.branches;
+
+    for (const chunk of pathChunks) {
+      let node = subTree.find(n => n.name === chunk);
+
+      if (!node) {
+        node = { name: chunk, dir: isDir, branches: [] };
+        subTree.push(node);
+      }
+
+      subTree = node.branches;
+    }
+  }
+
+  return tree;
+};
