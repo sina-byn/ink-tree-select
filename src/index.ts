f7a7299cd47ecd1d12199abd9d6e73156ccbae28
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-06 12:33:02 +0330

    refactor: barrel file exports fixed

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-export {
-  TreeSelect,
-  type TreeSelectProps,
-  type TreeSelectOptions,
-} from './components/TreeSelect.js';
+export { TreeSelect } from './components/TreeSelect.js';
 
-export {
-  VirtualTreeSelect,
-  type VirtualTreeSelectProps,
-  type VirtualTreeSelectOptions,
-} from './components/VirtualTreeSelect.js';
+export { VirtualTreeSelect } from './components/VirtualTreeSelect.js';
 
-export { type Tree } from './utils/index.js';
+export type {
+  Tree,
+  TreeSelectProps,
+  TreeSelectOptions,
+  VirtualTreeSelectProps,
+  VirtualTreeSelectOptions,
+} from './types/index.js';
